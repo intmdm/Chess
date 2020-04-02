5a5a469b5a65aea73828b6425bee3628f6948142
daniilmaksimcik <daniil.mm@icloud.com>
2020-04-02 13:59:25 +0300

    My initial project setup.

---
+++ Chess/Chess/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Chess
+//
+//  Created by Даниил Максимчик on 4/2/20.
+//  Copyright © 2020 Даниил Максимчик. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
