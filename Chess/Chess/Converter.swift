4901f823f8dcd8226b16b1ccb1e4be409f2a0a60
daniilmaksimcik <daniil.mm@icloud.com>
2020-04-06 21:25:51 +0300

    Problems with parsing Character to Int

--- Chess/Chess/Converter.swift
+++ Chess/Chess/Converter.swift
@@ -10,19 +10,25 @@ import Foundation
 import Cocoa
 
 class Converter {
-  var str: String
-
+  var str: String = ""
+  var mas: [Chessman] = []
+  
   init(str: String) {
     self.str = str
   }
     
-  func input() -> Void {
-    
+  func parsing() -> Void {
     var result: [String] =  str.components(separatedBy: ["\n"])
     for i in stride(from: 0, to: result.count / 2, by: 1) {
       result.remove(at: i)
     }
     print(result)
+    
+    for var temp in result {
+      if temp.count == 2 {
+        mas.append(Chessman(type: .pawn, color: .white, figure: "♙", coordinates:(String(temp[temp.startIndex]), Int(String(temp[temp.startIndex])))))
+      }
+    }
   }
-  
 }
+
